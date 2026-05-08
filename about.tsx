4970d8db2104a4bff4226d33c58160f0d79575c7
danman23m-cmyk <danman23m@gmail.com>
2026-05-07 20:09:25 -0500

    Add files via upload

---
+++ about.tsx
@@ -0,0 +1,10 @@
+
+export default function About() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>About</h1>
+      <p>AI toolkit for creators.</p>
+      <a href="/">Back</a>
+    </div>
+  );
+}
---
+++ blog.tsx
@@ -0,0 +1,10 @@
+
+export default function Blog() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Blog</h1>
+      <p>Coming soon...</p>
+      <a href="/">Back</a>
+    </div>
+  );
+}
